0659e9c89a040336e85e09de6953c2758af00ea4
Aicha DIAKITE <aicha.diak1994@gmail.com>
2017-10-19 13:01:01 +0200

    index.php a completer

--- index.php
+++ index.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<title>Page magique</title>
-	</head>
-	<body>
-		<div class="container">
-			<div id="requete">
-				<h2> Voici la page de lien vers la base de données</h2>
-				<textarea rows=10 cols=100>Entrez votre requête ici</textarea><br/>
-				<button class="btn">Valider</button>
-			</div>
-
-			<div id="resultat"></div>
-		</div>
-	</body>
-</html>
-
-
 <?php
 	$adress = '13.81.201.112';
 	$user = "DentegoRep";
 	$pwd = "DentegoRep2017";
 	$dbname = "dentegoreporting";
+	
+	$req = $_POST["req"];
+	$affichage = "";
 
 	$con = new mysqli($adress, $user, $pwd, $dbname);
 
@@ -31,17 +13,44 @@
 	    die("Connection failed: " . $con->connect_error);
 	} 
 	echo "Connected successfully";
-	$result = $con->query('SELECT * FROM ref_centre');
+	$result = $con->query($req);
 	if (!$result) 
 	{
 	    die('Requête invalide : ' . mysql_error());
 	}
 	else 
 	{
-		echo "yes";
-		echo $result->num_rows;
+		//faire une boucle sur les lignes du resultat de la requete avec mysqli_fetch_row
+		//pour chaque ligne tu fait un mysqli_fetch_array qui va recupérer la ligne sous forme de tableau
+		//ensuite concatener la chaine $affichage avec les colonnes du tableau en les séparant par des ;
 	}	
 
 	$result->close();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<meta charset="UTF-8" />
+		<title>Page magique</title>
+	</head>
+	<body>
+		<div class="container">
+			<form action="index.php" method="post">
+				<div id="requete">
+					<h2> Voici la page de lien vers la base de données</h2>
+					<textarea rows=10 cols=100 id="req" name="req">Entrez votre requête ici</textarea><br/>
+					<button class="btn" type="submit">Valider</button>
+				</div>
+			</form>
+			
+
+			<div id="resultat">
+				<!-- afficher la variable affichage de php -->
+			</div>
+		</div>
+	</body>
+</html>
